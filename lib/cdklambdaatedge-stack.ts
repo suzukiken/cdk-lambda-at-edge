ee292d67976ddcc287f33dbfc57a244cff4f292a
ken <ken@figment-research.com>
2020-12-13 09:23:07 +0000

    update

--- lib/cdklambdaatedge-stack.ts
+++ lib/cdklambdaatedge-stack.ts
@@ -3,23 +3,41 @@ import * as s3 from "@aws-cdk/aws-s3";
 import * as iam from "@aws-cdk/aws-iam";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as cloudfront from "@aws-cdk/aws-cloudfront";
-const sha256File = require("sha256-file");
+import * as ssm from "@aws-cdk/aws-ssm";
 
-/*
-npm install @aws-cdk/aws-s3
-npm install @aws-cdk/aws-iam
-npm install @aws-cdk/aws-lambda
-npm install @aws-cdk/aws-cloudfront
-npm install sha256-file
-*/
+const sha256File = require("sha256-file"); // npm install sha256-file
 
 export class CdklambdaatedgeStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    //-------------------------- import
+
+    // ssmパラメーターストアはCDKとリージョンが同じでないとアクセスできないので同じリージョンに置く
+
+    const role_arn = ssm.StringParameter.fromStringParameterName(
+      this,
+      "ssm_stringvalue_role_arn",
+      "cdkadmincognito-iam-authenticated-role"
+    ).stringValue;
+
+    const auth_role = iam.Role.fromRoleArn(
+      this,
+      "iam_authenticated_role",
+      role_arn
+    );
+
+    const bucket_name = ssm.StringParameter.fromStringParameterName(
+      this,
+      "ssm_stringvalue_bucket_name",
+      "lambdaatedge-derivery-bucket-name"
+    ).stringValue;
+
     const bucket = new s3.Bucket(this, "derivery-figment-research", {
-      bucketName: "derivery.figment-research.com",
+      bucketName: bucket_name,
     });
+
+    //-------------------------- lambda
 
     const resizer_function = new lambda.Function(this, "lambda_function", {
       runtime: lambda.Runtime.PYTHON_3_8,
@@ -41,6 +59,22 @@ export class CdklambdaatedgeStack extends cdk.Stack {
     const version = resizer_function.addVersion(
       sha256File("lambda/image-resizer.py")
     );
+
+    //-------------------------- permission
+
+    const iam_s3_policy_statement = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+    });
+
+    iam_s3_policy_statement.addActions("s3:GetObject", "s3:PutObject");
+    iam_s3_policy_statement.addResources(bucket.bucketArn);
+
+    const iam_s3_policy = new iam.Policy(this, "iam_s3_policy", {
+      policyName: "cdklambdaatedge-s3-policy",
+      statements: [iam_s3_policy_statement],
+    });
+
+    auth_role.attachInlinePolicy(iam_s3_policy);
 
     bucket.grantReadWrite(resizer_function);
 
